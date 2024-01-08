82c63b1bde9089cd790060ad9f9db41051437762
Molleigh <144710935+molleighH@users.noreply.github.com>
2024-01-08 16:53:46 -0500

    commit

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,6 +1,10 @@
 // Store constant URL variable 
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
+// Console log promise for debug purposes
+const dataPromise = d3.json(url)
+console.log("Data Promise: ", dataPromise)
+
 // Fetch JSON data and Console log it
 d3.json(url).then(function(data) {
     console.log(data);
@@ -9,37 +13,65 @@ d3.json(url).then(function(data) {
 // Initialize 
 function init() {
 
-    // D3 to select dropdownMenu
+    // Use D3 to select dropdownMenu
     let dropdownMenu = d3.select("#selDataset");
+    
+    // Use D3 to retrieve sample names & populate drowdown selector
+    d3.json(url).then((data) => {
 
-     // Use D3 for dropdown selector
-     d3.json(url).then((data) => {
-        
         // Set variable for sample names
         let names = data.names;
 
         // Add samples to dropdownMenu
-        names.forEach((id) => {
-
-            // Log value of ID for each iteration of the loop
+        names.forEach(id) => {
+            
+            // Log value of id for each iteration of loop
             console.log(id);
 
-            dropdownMenu.append("option")
-            .text(id)
-            .property("value",id);
+            dropdownMenu.append("option").text(id).property("value", id);
         });
 
-        // Set first sample from list
-        let first_sample = names[0];
+        // Set first entry from data list
+        let firstEntry = names[0];
 
-        // Log value of sample_zero
-        console.log(first_sample);
+        // Log value of firstEntry
+        console.log(firstEntry);
 
-        // Build the initial plots
-        buildMetadata(first_sample);
-        buildBarChart(first_sample);
-        buildBubbleChart(first_sample);
-        buildGaugeChart(first_sample);
-
+        //Build initial plots
+        buildMetadata(firstEntry);
+        buildBarChart(firstEntry);
+        buildBubbleChart(firstEntry);
     });
 };
+
+// Function to populate Metadata 
+function buildMetadata(sample) {
+
+    // Use D3 to retrieve all of data
+    d3.json(url).then((data) => {
+
+        // Retrieve all metadata
+        let metadata = data.metadata;
+
+        // Filter metadata for sample ID
+        let value = metadata.filter(result => result.id == sample);
+        console.log(value)
+
+        // Retrieve first index from array
+        let valueData = value[0];
+
+        // Clear out metadata
+        d3.select("#sample-metadata").html("");
+
+        // Use Object.entries to append each key & value pair to the panel
+        Object.entries(valueData).forEach(([key, value]) => {
+            console.log(key, value);
+            d3.select("#sample-metadata").append("h5").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+};
+
+// Function to build Bar Chart
+function buildBarChart(sample) {
+    // Use D3 to retrieve all of data
+    d3.json(url).then((data) => {
